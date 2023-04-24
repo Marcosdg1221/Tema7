c1e6e9cc87637d5f4817e00745a281e336021bf0
mdominguez <mdominguez@106-31.info2>
2023-04-24 13:41:57 +0200

    k

--- Tema7/src/crud1/Main.java
+++ Tema7/src/crud1/Main.java
@@ -1,18 +1,7 @@
 package crud1;
 
-import java.util.Arrays;
-
 public class Main {
 	public static void main(String[] args) {
-	Main art = new Main();
-	alta(art);
 	
-	System.out.println(Arrays.toString(numeros));
 	}
 }
-
-public static void alta (Main a, Main ) {
-	
-}
-
-public static void 
